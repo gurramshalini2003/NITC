c3aa58dc6acc36576245f40b9b8db7590170d79e
gurramshalini2003 <gurramshalini2003@gmail.com>
2021-03-31 13:41:29 +0530

    IC

---
+++ README.md
@@ -0,0 +1,1 @@
+The school to college transition is difficult as it is; without the added burden of a pandemic. “Freshers Zone” was created to make this transition as smooth as possible. We want for “Freshers Zone” to be the resource for all freshers, the resource we never had.
---
+++ js/script.js
@@ -0,0 +1,12 @@
+(function($) {
+
+	"use strict";
+		// fixed-menu
+		$(window).on('scroll', function () {
+			if ($(window).scrollTop() > 50) {
+				$('.top-nav').addClass('fixed-menu');
+			} else {
+				$('.top-nav').removeClass('fixed-menu');
+			}
+		});
+})(window.jQuery);
